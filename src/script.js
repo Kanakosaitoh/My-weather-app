e8857b95764ae1584bd5847153765a488eb006ca
Kanakosaitoh <kanahanasasha@gmail.com>
2025-01-15 13:53:21 -0800

    added function search

--- src/script.js
+++ src/script.js
@@ -1,1 +1,8 @@
-alert("hi");
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let currentCity = document.querySelector("#current-city");
+  currentCity.innerHTML = searchInputElement.value;
+}
+let cityElement = document.querySelector("#search-form");
+cityElement.addEventListener("submit", search);
