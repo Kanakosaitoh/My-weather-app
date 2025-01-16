f9a4ed8e78b3d86a24affbe060d4201c1026fb51
Kanakosaitoh <kanahanasasha@gmail.com>
2025-01-16 11:16:18 -0800

    added function refreshWeather

--- src/script.js
+++ src/script.js
@@ -1,8 +1,30 @@
+function refreshWeather(response) {
+  let currentTemp = document.querySelector("#current-temperature-value");
+  currentTemp.innerHTML = Math.round(response.data.temperature.current);
+  let currentHumidity = document.querySelector("#current-humidity");
+  currentHumidity.innerHTML = Math.round(response.data.temperature.humidity);
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+  let windElement = document.querySelector("#current-wind");
+  windElement.innerHTML = response.data.wind.speed;
+  let weatherElement = document.querySelector("#current-sky");
+  weatherElement.innerHTML = response.data.condition.description;
+}
+
+function searchCity(city) {
+  let apiKey = "821b2a703ac1t98f96395477546f6ebo";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
   let currentCity = document.querySelector("#current-city");
   currentCity.innerHTML = searchInputElement.value;
+  searchCity(searchInputElement.value);
 }
 let cityElement = document.querySelector("#search-form");
 cityElement.addEventListener("submit", search);
+
+searchCity("Tokyo");
